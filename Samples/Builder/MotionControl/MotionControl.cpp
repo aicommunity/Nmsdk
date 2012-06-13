ab1359131e284a68abd73e440f2a6a3b59e7c294
Alexab <devnull@localhost>
2012-06-13 14:29:40 +0400

    ATTENTION!! Initiated changes to the file structure.

--- Samples/Builder/MotionControl/MotionControl.cpp
+++ Samples/Builder/MotionControl/MotionControl.cpp
@@ -45,27 +45,28 @@
 
 
 
+
 USEFORM("..\..\..\RDK\GUI\Builder\UEngineMonitorFormUnit.cpp", UEngineMonitorForm);
 USEFORM("..\..\..\RDK\GUI\Builder\UEngineMonitorFrameUnit.cpp", UEngineMonitorFrame); /* TFrame: File Type */
-USEFORM("..\..\..\RDK\GUI\Builder\UEngineControlFormUnit.cpp", UEngineControlForm);
+USEFORM("..\..\..\RDK\GUI\Builder\UListInputFormUnit.cpp", UListInputForm);
 USEFORM("..\..\..\RDK\GUI\Builder\UComponentsPerformanceFormUnit.cpp", UComponentsPerformanceForm);
 USEFORM("..\..\..\RDK\GUI\Builder\UComponentsPerformanceFrameUnit.cpp", UComponentsPerformanceFrame); /* TFrame: File Type */
+USEFORM("..\..\..\RDK\GUI\Builder\UEngineControlFormUnit.cpp", UEngineControlForm);
+USEFORM("..\..\..\RDK\GUI\Builder\USeriesControlUnit.cpp", USeriesControlForm);
 USEFORM("MainFormUnit.cpp", MainForm);
-USEFORM("..\..\..\RDK\GUI\Builder\UComponentsListFrameUnit.cpp", UComponentsListFrame); /* TFrame: File Type */
-USEFORM("..\..\..\RDK\GUI\Builder\UClassesListFormUnit.cpp", UClassesListForm);
-USEFORM("..\..\..\RDK\GUI\Builder\UClassesListFrameUnit.cpp", UClassesListFrame); /* TFrame: File Type */
-USEFORM("..\..\..\GUI\Builder\TNDCEngineControlFormUnit.cpp", NDCEngineControlForm);
-USEFORM("..\..\..\GUI\Builder\TNManipulatorControlForm.cpp", NManipulatorControlForm);
-USEFORM("..\..\..\RDK\GUI\Builder\UComponentIOFrameUnit.cpp", UComponentIOFrame); /* TFrame: File Type */
-USEFORM("..\..\..\RDK\GUI\Builder\UComponentsControlFrameUnit.cpp", UComponentsControlFrame); /* TFrame: File Type */
-USEFORM("..\..\..\RDK\GUI\Builder\UComponentsListFormUnit.cpp", UComponentsListForm);
-USEFORM("..\..\..\RDK\GUI\Builder\UComponentsControlFormUnit.cpp", UComponentsControlForm);
-USEFORM("..\..\..\RDK\GUI\Builder\UComponentLinksFormUnit.cpp", UComponentLinksForm);
-USEFORM("..\..\..\RDK\GUI\Builder\UComponentLinksFrameUnit.cpp", UComponentLinksFrame); /* TFrame: File Type */
 USEFORM("..\..\..\RDK\GUI\Builder\UWatchFrameUnit.cpp", UWatchFrame); /* TFrame: File Type */
 USEFORM("..\..\..\RDK\GUI\Builder\UWatchWindowFormUnit.cpp", UWatchWindowForm);
-USEFORM("..\..\..\RDK\GUI\Builder\USeriesControlUnit.cpp", USeriesControlForm);
-USEFORM("..\..\..\RDK\GUI\Builder\UListInputFormUnit.cpp", UListInputForm);
+USEFORM("..\..\..\RDK\GUI\Builder\UComponentsListFrameUnit.cpp", UComponentsListFrame); /* TFrame: File Type */
+USEFORM("..\..\..\RDK\GUI\Builder\UClassesListFormUnit.cpp", UClassesListForm);
+USEFORM("..\..\..\GUI\Builder\TNDCEngineControlFormUnit.cpp", NDCEngineControlForm);
+USEFORM("..\..\..\GUI\Builder\TNManipulatorControlForm.cpp", NManipulatorControlForm);
+USEFORM("..\..\..\RDK\GUI\Builder\UClassesListFrameUnit.cpp", UClassesListFrame); /* TFrame: File Type */
+USEFORM("..\..\..\RDK\GUI\Builder\UComponentsControlFormUnit.cpp", UComponentsControlForm);
+USEFORM("..\..\..\RDK\GUI\Builder\UComponentsControlFrameUnit.cpp", UComponentsControlFrame); /* TFrame: File Type */
+USEFORM("..\..\..\RDK\GUI\Builder\UComponentsListFormUnit.cpp", UComponentsListForm);
+USEFORM("..\..\..\RDK\GUI\Builder\UComponentIOFrameUnit.cpp", UComponentIOFrame); /* TFrame: File Type */
+USEFORM("..\..\..\RDK\GUI\Builder\UComponentLinksFormUnit.cpp", UComponentLinksForm);
+USEFORM("..\..\..\RDK\GUI\Builder\UComponentLinksFrameUnit.cpp", UComponentLinksFrame); /* TFrame: File Type */
 //---------------------------------------------------------------------------
 WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 {
