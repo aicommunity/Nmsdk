f2ee0a7b56dcf35f0792bb7714ec7e2773e7072b
Alexab <palexab@gmail.com>
2014-07-01 17:46:29 +0400

    Dummy typo fixed.

--- GUI/BCB/TNManipulatorControlForm.cpp
+++ GUI/BCB/TNManipulatorControlForm.cpp
@@ -204,7 +204,7 @@ void TNManipulatorControlForm::AUpdateInterface(void)
 // Создание копии этого компонента
 TUVisualControllerForm* TNManipulatorControlForm::New(TComponent *owner)
 {
- return TNManipulatorControlForm(owner);
+ return new TNManipulatorControlForm(owner);
 }
 
 
