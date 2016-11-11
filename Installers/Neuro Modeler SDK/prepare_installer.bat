841b4701a04f19a7f049603395860e4402dce683
Alexab <devnull@localhost>
2016-11-11 15:51:35 +0300

    Installer has been updated.

--- Installers/Neuro Modeler SDK/prepare_installer.bat
+++ Installers/Neuro Modeler SDK/prepare_installer.bat
@@ -4,5 +4,15 @@ xcopy "..\..\Bin\Platform\Win\*.dll" InstallSources\Bin\Platform\Win\ /Y /D
 xcopy "..\..\Bin\Platform\Win\*.ini" InstallSources\Bin\Platform\Win\ /Y /D
 xcopy "..\..\Bin\Platform\Win\Fonts\*.*" InstallSources\Bin\Platform\Win\Fonts\ /Y /D
 xcopy "..\..\Bin\Configs\MC-RCN\*.*" InstallSources\Bin\Configs\MC-RCN\*.* /Y /E /D
+xcopy "..\..\Bin\Configs\NM-Neurons\*.*" InstallSources\Bin\Configs\NM-Neurons\*.* /Y /E /D
+xcopy "..\..\Bin\Configs\MC-Muscles\*.*" InstallSources\Bin\Configs\MC-Muscles\*.* /Y /E /D
+xcopy "..\..\Docs\NMSDK.NeuronModel.ConfigurationSamples.Ru.doc" InstallSources\Docs\NMSDK.NeuronModel.ConfigurationSamples.Ru.doc /Y /D
+xcopy "..\..\Docs\NMSDK.NeuronModel.Ru.doc" InstallSources\Docs\NMSDK.NeuronModel.Ru.doc /Y /D
+xcopy "..\..\Rdk\Docs\RDK.GuiDescription.Bcb.Ru.doc" InstallSources\Docs\RDK.GuiDescription.Bcb.Ru.doc /Y /D
+xcopy "..\..\Rdk\Docs\RDK.RuntimeConfigurationDescription.Ru.doc" InstallSources\Docs\RDK.RuntimeConfigurationDescription.Ru.doc /Y /D
 rem rmdir /Q /S InstallSources\Bin\Configs\MC-RCN\ImageLog
 rem rmdir /Q /S InstallSources\Bin\Configs\MC-RCN\EventsLog
+rem rmdir /Q /S InstallSources\Bin\Configs\NM-Neurons\ImageLog
+rem rmdir /Q /S InstallSources\Bin\Configs\NM-Neurons\EventsLog
+rem rmdir /Q /S InstallSources\Bin\Configs\MC-Muscles\ImageLog
+rem rmdir /Q /S InstallSources\Bin\Configs\MC-Muscles\EventsLog
