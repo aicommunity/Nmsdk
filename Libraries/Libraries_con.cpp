9c4ebd23d1ec4e7ec9fdba69b57ff0e5391d338d
Alexab <devnull@localhost>
2018-03-25 11:28:40 +0300

    Files added.

---
+++ Libraries/Libraries_con.cpp
@@ -0,0 +1,52 @@
+﻿#include "Libraries.h"
+
+namespace RDK {
+
+/// Функция должна быть реализована в конечном проекте и загружать требуемые библиотеки
+bool RDK_CALL RdkLoadPredefinedLibraries(std::list<RDK::ULibrary*> &libs_list)
+{
+ using namespace NMSDK;
+// RDK::UItemData::DefaultDataSize=sizeof(double);
+ libs_list.push_back(&RDK::BCLLibrary);
+ libs_list.push_back(&RDK::IOLibrary);
+ libs_list.push_back(&RDK::CRLibrary);
+ libs_list.push_back(&RDK::StatisticLibrary);
+ libs_list.push_back(&RDK::SourceLibrary);
+ libs_list.push_back(&RDK::RDKVideoCaptureLibrary);
+ libs_list.push_back(&RDK::AriphmeticLibrary);
+ libs_list.push_back(&RDK::BasicLibrary);
+ libs_list.push_back(&RDK::DetectionLibrary);
+ libs_list.push_back(&RDK::GUILibrary);
+ libs_list.push_back(&RDK::SimulatorLibrary);
+ libs_list.push_back(&RDK::BStatisticLibrary);
+ libs_list.push_back(&RDK::NoiseLibrary);
+
+
+ libs_list.push_back(&NMSDK::BCLLibrary);
+ libs_list.push_back(&NMSDK::ActLibrary);
+ libs_list.push_back(&NMSDK::MathLibrary);
+ libs_list.push_back(&NMSDK::PhysLibrary);
+ libs_list.push_back(&NMSDK::SensorLibrary);
+ libs_list.push_back(&NMSDK::NeuronSourceLibrary);
+ libs_list.push_back(&NMSDK::ReceiverLibrary);
+ libs_list.push_back(&NMSDK::PulseLibrary);
+ libs_list.push_back(&NMSDK::NeuronLifeLibrary);
+ //libs_list.push_back(&NMSDK::PulseNeuronLibrary);
+ #ifndef NMSDK_DEBUG
+ //libs_list.push_back(&NMSDK::MotionControlLibrary);
+ #endif
+ #ifndef __GNUC__
+ libs_list.push_back(&NMSDK::WinAPIActLibrary);
+ #endif
+ libs_list.push_back(&NMSDK::PulseClassicModelsLib);
+
+ return true;
+}
+
+/// Функция должна быть реализована в конечном проекте
+bool RDK_CALL RdkCreatePredefinedStructure(RDK::UEnvironment* env, int predefined_structure)
+{
+ return true;
+}
+
+}
---
+++ Libraries/Libraries_con.h
@@ -0,0 +1,43 @@
+﻿#ifndef NMSDK_LIBRARIES_H
+#define NMSDK_LIBRARIES_H
+
+#include "../Rdk/Deploy/Include/rdk.h"
+
+#include "Rdk-BasicLib/Deploy/Include/Lib.h"
+#include "Rdk-BasicStatisticLib/Deploy/Include/Lib.h"
+#include "Rdk-IoLib/Deploy/Include/Lib.h"
+#include "Rdk-SourcesLib/Deploy/Include/Lib.h"
+#include "Rdk-CvVideoCaptureLib/Core/VideoCap.h"
+#include "Rdk-CvAriphmeticLib/Deploy/Include/Lib.h"
+#include "Rdk-CvBasicLib/Deploy/Include/Lib.h"
+#include "Rdk-CvDetectionLib/Deploy/Include/Lib.h"
+#include "Rdk-CvGuiLib/Deploy/Include/Lib.h"
+#include "Rdk-CvStatisticLib/Deploy/Include/Lib.h"
+#include "Rdk-CvSimulatorLib/Deploy/Include/Lib.h"
+#include "Rdk-CRLib/Deploy/Include/Lib.h"
+#include "Rdk-NoiseGenLib/Deploy/Include/Lib.h"
+#include "Rdk-CvVideoCaptureLib/Deploy/Include/Lib.h"
+#include "Nmsdk-ActLib/Deploy/Include/Lib.h"
+#include "Nmsdk-BasicLib/Deploy/Include/Lib.h"
+#include "Nmsdk-MathLib/Deploy/Include/Lib.h"
+//#include "Nmsdk-MotionControlLib/Deploy/Include/Lib.h"
+#include "Nmsdk-NeuronLifeLib/Deploy/Include/Lib.h"
+#include "Nmsdk-PhysLib/Deploy/Include/Lib.h"
+#include "Nmsdk-PulseLib/Deploy/Include/Lib.h"
+//#include "Nmsdk-PulseNeuronLib/Deploy/Include/Lib.h"
+#include "Nmsdk-ReceiverLib/Deploy/Include/Lib.h"
+#include "Nmsdk-SensorLib/Deploy/Include/Lib.h"
+#include "Nmsdk-SourceLib/Deploy/Include/Lib.h"
+#include "Nmsdk-PulseClassicModelsLib/Core/Lib.h"
+
+namespace RDK {
+
+/// Функция должна быть реализована в конечном проекте и загружать требуемые библиотеки
+extern RDK_LIB_TYPE bool RDK_CALL RdkLoadPredefinedLibraries(std::list<RDK::ULibrary*> &libs_list);
+
+/// Функция должна быть реализована в конечном проекте
+extern RDK_LIB_TYPE bool RDK_CALL RdkCreatePredefinedStructure(RDK::UEnvironment* env, int predefined_structure);
+
+}
+
+#endif
