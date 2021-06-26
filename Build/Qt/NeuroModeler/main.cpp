f51d65e822e612592426342c9a0f11ed81f36ad0
Alexab <devnull@localhost>
2021-06-26 21:53:19 +0300

    Gui/Qt: Launching application progress bar have been added.

--- Build/Qt/NeuroModeler/main.cpp
+++ Build/Qt/NeuroModeler/main.cpp
@@ -5,11 +5,29 @@
 #include "../../../Rdk/Core/Utilities/UIniFile.h"
 #include "../../../Rdk/Core/Application/Qt/UProjectDeployerQt.h"
 
+QProgressDialog* d(NULL);
+
+void progress_bar_callback(int complete_percent)
+{
+ if(d)
+ {
+  d->setValue(complete_percent);
+  QApplication::processEvents();
+ }
+}
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    RDK::UAppCore<RDK::UApplication, UEngineControlQt, RDK::UProject, RDK::UServerControl, RDK::UTestManager, RDK::URpcDispatcher, RDK::URpcDecoderInternal, RDK::URpcDecoderCommon, UServerTransportTcpQt, RDK::UProjectDeployerQt> AppCore;
+    d=new QProgressDialog;
+    d->setLabelText("Launching application");
+    d->setMaximum(100);
+    d->setValue(10);
+    d->show();
+    QApplication::processEvents();
+
+    RDK::UAppCore<RDK::UApplication, UEngineControlQt, RDK::UProject, RDK::UServerControl, RDK::UTestManager, RDK::URpcDispatcher, RDK::URpcDecoderInternal, RDK::URpcDecoderCommon, UServerTransportTcpQt, RDK::UProjectDeployerQt> AppCore(progress_bar_callback);
 
      int init_res=AppCore.Init(QApplication::applicationFilePath().toLocal8Bit().constData(), "NeuroModeler.ini",
                   (QApplication::applicationDirPath()+"/EventsLog/").toLocal8Bit().constData(),
@@ -30,5 +48,9 @@ int main(int argc, char *argv[])
     if(AppCore.startMinimized)
       w.showMinimized();
 
+    d->setValue(100);
+    d->hide();
+    delete d;
+    d=NULL;
     return a.exec();
 }
