f9d1bd5785ef196188e8c106e24508b3d5c6d156
Alexab <palexab@gmail.com>
2024-10-12 12:12:34 +0300

    fix origins

--- PubUpdate.bat
+++
@@ -1,4 +0,0 @@
-path "c:\program files\winrar\"
-rar.exe f -r ..\Publishing\nmsdk_ailabru_full.rar 
-rar.exe f -r ..\Publishing\nmsdk_ailabru_srconly.rar 
-rar.exe f -r ..\Publishing\nmsdk_ailabru_minimum.rar 
